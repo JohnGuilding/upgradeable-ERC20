2dc34cc0b7acf81da858e3d62d0ea6d867685c8f
JohnGuilding <jwguilding@gmail.com>
2022-03-31 13:30:05 +0100

    Move interface to separate file

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -3,27 +3,7 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-
-interface IERC20 {
-    function totalSupply() external view returns (uint);
-
-    function balanceOf(address account) external view returns (uint);
-
-    function transfer(address recipient, uint amount) external returns (bool);
-
-    function allowance(address owner, address spender) external view returns (uint);
-
-    function approve(address spender, uint amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-}
+import "./IERC20.sol";
 
 contract ERC20 is Initializable, IERC20, OwnableUpgradeable {
     uint public _totalSupply;
